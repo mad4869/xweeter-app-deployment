7dbe2e56bf11c8601a4f26165eff046b0a689f90
MAkbarD <mad4869@gmail.com>
2024-01-07 05:40:51 +0800

    Restore logging

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,5 +1,12 @@
 from flask import Blueprint
+import logging
 
 routes = Blueprint("routes", __name__)
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 from . import auth, users, xweets, follows, rexweets, timeline, likes, replies, hashtags
